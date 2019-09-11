03e3562b4fde426fb093728f101a9394f9aa5355
Labanics Gergő <Labanics Gerg@DESKTOP-QFEVJGT>
2019-09-11 19:50:25 +0200

    Feladat 1 kész

---
+++ LAB01_KonzolosHelloWorld/Tests/PrimeTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Numbers;
+
+namespace Tests
+{
+    public class PrimeTests
+    {
+        [Fact]
+        public void CountPrimeNumbers100Returns25()
+        {
+            var p = new CountPrimeNumbers(100);
+            Assert.Equal(25, p.CalculateSolution());
+
+        }
+    }
+}
